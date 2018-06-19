29552bea52bc17056eac5b5024c27ad3bc3e06f5
aspeziale <aspeziale@digitalidentity.tech>
2018-06-19 13:20:41 +0200

    Limiting the deploy to the Eidoo private chain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,31 +3,38 @@ const SpaghettiSale = artifacts.require('./SpaghettiSale.sol')
 
 module.exports = (deployer, network, accounts) => {
     
-    const wallet      = accounts[1]
-    const openingTime = web3.eth.getBlock('latest').timestamp + 10  // Starts after 10 seconds in the future
-    const closingTime = openingTime + 86400 * 20                    // Ends after 20 days
-    const rate        = new web3.BigNumber(1000)
-    const totalTokens  = 10 * 10^21                                  // 10K Ether in Wei
+    if(network === 'eidoo') {
+        const wallet            = accounts[1]                                 // Where funds and token are stored
+        const stagingKycSigners = [                                           // KYC Signers
+            '0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef', 
+            '0x890d4c6b94e6f54bdbb58530f425c2a5a3033361'
+        ]                                    
+        const openingTime       = web3.eth.getBlock('latest').timestamp       // Starts immediately
+        const closingTime       = openingTime + 86400 * 20                    // Ends after 20 days
+        const totalTokens       = 1000000*10**18                              // 1 mln of tokens
+        const rate              = 5                                           // Multiplier 1 ether gives you back 5 token
 
-    return deployer
-        .then(() => {
-            return deployer.deploy(SpaghettiCoin, totalTokens, {from: wallet})
-        })
-        .then((instance) => {
-            SpaghettiCoinInst = instance
-            return deployer.deploy(
-                SpaghettiSale,
-                SpaghettiCoin.address,
-                wallet,
-                openingTime,
-                closingTime,
-                rate,
-                totalTokens,
-                {from: wallet}
-            )
-        })
-        .then((instance) => {
-            SpaghettiSaleInst = instance
-            return SpaghettiCoinInst.approve(wallet, totalTokens);
-        })
+        return deployer
+            .then(() => {
+                return deployer.deploy(SpaghettiCoin, totalTokens, {from: wallet})
+            })
+            .then((instance) => {
+                SpaghettiCoinInst = instance
+                return deployer.deploy(
+                    stagingKycSigners,
+                    SpaghettiSale,
+                    SpaghettiCoin.address,
+                    wallet,
+                    openingTime,
+                    closingTime,
+                    rate,
+                    totalTokens,
+                    {from: wallet}
+                )
+            })
+            .then((instance) => {
+                SpaghettiSaleInst = instance
+                return SpaghettiCoinInst.approve(wallet, totalTokens);
+            })
+    }
 }
