e245e4476af61595dcbbba1ed447793c78a8a12b
aspeziale <aspeziale@digitalidentity.tech>
2018-08-01 16:40:47 +0200

    Clean networks

--- truffle.js
+++ truffle.js
@@ -1,3 +1,11 @@
+// const Web3 = require('web3')
+
+// const getWeb3Instance = (host, port, protocol) => {
+//   web3 = new Web3(new Web3.providers.HttpProvider(protocol + '://' + host + ':' + port))
+//   console.log(`[live gasPrice]: ${web3.eth.gasPrice.toNumber()}`)
+//   return web3
+// }
+
 module.exports = {
   solc: {
     optimizer: {
@@ -6,19 +14,26 @@ module.exports = {
     }
   },
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 6721975,
-      gasPrice: 65000000000,
+    development: { // npm run ganache
+      host: 'localhost',
+      port: 7545,
+      network_id: '8994',
+      gas: 800000,
+      gasPrice: 2000000000
     },
-    eidoo: {
-      host: "wally-api-dev.undo.it",
+    parity: { // private parity remote node
+      host: 'eidoo-dev-1.bchainapi.net', //wally-api-dev.undo.it',
       port: 8545,
-      gas: 6721975,
-      gasPrice: 65000000000,
-      network_id: "*" // Match any network id
+      gas: 800000,
+      gasPrice: 22000000000,
+      network_id: '8995'
+    }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions : {
+      currency: 'EUR',
+      gasPrice: 22,
     }
   }
 }
