ba8692495c04377069e222bb37bbf80d33238f4c
andreafspeziale <andreaf.speziale@gmail.com>
2018-08-01 16:39:42 +0200

    Clean truffle networks

--- truffle.js
+++ truffle.js
@@ -1,3 +1,11 @@
+// const Web3 = require('web3')
+
+// const getWeb3Instance = (host, port, protocol) => {
+//   web3 = new Web3(new Web3.providers.HttpProvider(protocol + '://' + host + ':' + port))
+//   console.log(`[live gasPrice]: ${web3.eth.gasPrice.toNumber()}`)
+//   return web3
+// }
+
 module.exports = {
   solc: {
     optimizer: {
@@ -6,40 +14,34 @@ module.exports = {
     }
   },
   networks: {
-    development: { // $ ganache-cli --port 7545
+    development: { // npm run ganache
       host: 'localhost',
       port: 7545,
-      network_id: '*', // Match any network id
-      gas: 6721975,
-      gasPrice: 65000000000,
+      network_id: '8994',
+      gas: 800000,
+      gasPrice: 2000000000
     },
     parity: { // private parity remote node
       host: 'eidoo-dev-1.bchainapi.net', //wally-api-dev.undo.it',
       port: 8545,
-      gas: 4700000,
-      gasPrice: 65000000000,
+      gas: 800000,
+      gasPrice: 22000000000,
       network_id: '8995'
     },
-    docker: { // private parity local node
-      host: 'localhost',
+    mainet: {
+      host: 'localhost', // local parity mainet node
       port: 8545,
-      gas: 4700000,
-      gasPrice: 65000000000,
-      network_id: '8996'
-    },
-    ropsten: { // private parity ropsten node
-      host: '206.189.124.70',
-      port: 8545,
-      gas: 4700000,
-      gasPrice: 65000000000,
-      network_id: '3'
-    },
+      gas: 800000,
+      gasPrice: 2000000000,
+      network_id: '1',
+      from: ''
+    }
   },
   mocha: {
     reporter: 'eth-gas-reporter',
     reporterOptions : {
       currency: 'EUR',
-      gasPrice: 21
+      gasPrice: 2,
     }
   }
 }
