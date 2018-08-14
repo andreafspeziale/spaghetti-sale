8c55bff5fd55cc3f185f7eabe59fac6b3d26329e
andreafspeziale <andreaf.speziale@gmail.com>
2018-08-14 23:29:51 +0200

    Fix indentation

--- contracts/SpaghettiCoin.sol
+++ contracts/SpaghettiCoin.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.24;
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract SpaghettiCoin is StandardToken {
-    string public name = "SPAGHETTI COIN";
-    string public symbol = "SPAG";
-    uint8 public decimals = 18;
-    
-    constructor(uint256 _totalSupply) public {
-        totalSupply_ = _totalSupply;
-        balances[msg.sender] = _totalSupply;
-    }
+  string public name = "SPAGHETTI COIN";
+  string public symbol = "SPAG";
+  uint8 public decimals = 18;
+
+  constructor(uint256 _totalSupply) public {
+    totalSupply_ = _totalSupply;
+    balances[msg.sender] = _totalSupply;
+  }
 }
--- contracts/SpaghettiSale.sol
+++ contracts/SpaghettiSale.sol
@@ -4,77 +4,77 @@ import "./SpaghettiCoin.sol";
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract SpaghettiSale {
-    using SafeMath for uint;
+  using SafeMath for uint;
 
-    SpaghettiCoin public token;
-    address public wallet;
+  SpaghettiCoin public token;
+  address public wallet;
 
-    uint public price;
+  uint public price;
 
-    uint public startTime;
+  uint public startTime;
 
-    uint public endTime;
+  uint public endTime;
 
-    uint public totalTokens;
+  uint public totalTokens;
 
-    uint public remainingTokens;
+  uint public remainingTokens;
 
-    constructor(
-        address _token, 
-        address _wallet, 
-        uint _startTime, 
-        uint _endTime, 
-        uint _price, 
-        uint _totalTokens)
-        public
-    {
-        token = SpaghettiCoin(_token);
-        wallet = _wallet;
-        startTime = _startTime;
-        endTime = _endTime;
-        price = _price;
-        totalTokens = _totalTokens;
-        remainingTokens = _totalTokens;
-    }
+  constructor(
+      address _token,
+      address _wallet,
+      uint _startTime,
+      uint _endTime,
+      uint _price,
+      uint _totalTokens)
+      public
+  {
+    token = SpaghettiCoin(_token);
+    wallet = _wallet;
+    startTime = _startTime;
+    endTime = _endTime;
+    price = _price;
+    totalTokens = _totalTokens;
+    remainingTokens = _totalTokens;
+  }
 
-    function releaseTokensTo(address buyer) internal returns(bool) {
-        require(now >= startTime && now < endTime);
-        uint amount = msg.value.mul(price);
-        remainingTokens = remainingTokens.sub(amount);
-        wallet.transfer(msg.value);
-        require(token.transferFrom(wallet, buyer, amount));
-        return true;
-    }
+  function releaseTokensTo(address buyer) internal returns(bool) {
+    require(now >= startTime && now < endTime);
+    uint amount = msg.value.mul(price);
+    remainingTokens = remainingTokens.sub(amount);
+    wallet.transfer(msg.value);
+    require(token.transferFrom(wallet, buyer, amount));
+    return true;
+  }
 
-    function started() public view returns(bool) {
-        return now >= startTime;
-    }
+  function started() public view returns(bool) {
+    return now >= startTime;
+  }
 
-    function ended() public view returns(bool) {
-        return now >= endTime || remainingTokens == 0;
-    }
+  function ended() public view returns(bool) {
+    return now >= endTime || remainingTokens == 0;
+  }
 
-    function startTime() public view returns(uint) {
-        return startTime;
-    }
+  function startTime() public view returns(uint) {
+    return startTime;
+  }
 
-    function endTime() public view returns(uint) {
-        return endTime;
-    }
+  function endTime() public view returns(uint) {
+    return endTime;
+  }
 
-    function totalTokens() public view returns(uint) {
-        return totalTokens;
-    }
+  function totalTokens() public view returns(uint) {
+    return totalTokens;
+  }
 
-    function remainingTokens() public view returns(uint) {
-        return remainingTokens;
-    }
+  function remainingTokens() public view returns(uint) {
+    return remainingTokens;
+  }
 
-    function price() public view returns(uint) {
-        return price;
-    }
+  function price() public view returns(uint) {
+    return price;
+  }
 
-    function() public payable {
-        releaseTokensTo(msg.sender);
-    }
+  function() public payable {
+    releaseTokensTo(msg.sender);
+  }
 }
